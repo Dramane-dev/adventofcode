a13d0b80667eb7d2c1e62453ee6b69afea940865
Dramane-dev <59121678+Dramane-dev@users.noreply.github.com>
2025-08-20 12:50:50 +0200

    feat(day-01): resolve day-01

---
+++ 2015/days/day-01/index.ts
@@ -0,0 +1,17 @@
+import { INPUT } from './input';
+import { getDeliveryFloor, getFirstDownInstructionIndex } from './solution';
+
+const puzzleResult = getDeliveryFloor(INPUT.instructions);
+const puzzleTitle = '--- Day 1: Not Quite Lisp ---';
+
+console.log({
+  title: '--- Day 1: Not Quite Lisp ---',
+  result: puzzleResult,
+});
+
+const puzzlePart2Result = getFirstDownInstructionIndex(INPUT.instructions);
+
+console.log({
+  title: '--- Day 1: Not Quite Lisp - Part 2 ---',
+  result: puzzlePart2Result,
+});
