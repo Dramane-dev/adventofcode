61343f58c0f66afe9cedb4cba248e056f12d66da
Dramane-dev <59121678+Dramane-dev@users.noreply.github.com>
2025-11-09 16:52:49 +0100

    feat(day-04): resolve day-04-part-2

--- 2015/days/day-04/index.ts
+++ 2015/days/day-04/index.ts
@@ -1,7 +1,7 @@
 import { INPUT } from './input';
 import { findSantaLowestPositiveNumber } from './solution';
 
-const puzzleResult = findSantaLowestPositiveNumber(INPUT);
+const puzzleResult = findSantaLowestPositiveNumber({ input: INPUT });
 
 console.log({
   title: '--- Day 4: The Ideal Stocking Stuffer ---',
