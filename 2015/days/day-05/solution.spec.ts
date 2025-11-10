cb350ec1e86e1f36f925bf03568e774d7d71a89e
Dramane-dev <59121678+Dramane-dev@users.noreply.github.com>
2025-11-10 22:17:31 +0100

    feat(day-05): resolve day-05-part-2

--- 2015/days/day-05/solution.spec.ts
+++ 2015/days/day-05/solution.spec.ts
@@ -1,4 +1,4 @@
-import { findHowManyNiceStrings } from './solution';
+import { findHowManyNiceStrings, findHowManyNiceStringsNext } from './solution';
 
 describe("Day 5: Doesn't He Have Intern-Elves For This?", () => {
   describe('findHowManyNiceStrings', () => {
@@ -28,6 +28,39 @@ describe("Day 5: Doesn't He Have Intern-Elves For This?", () => {
         // GIVEN
         // WHEN
         const result = findHowManyNiceStrings(input);
+
+        // THEN
+        expect(result).toBe(expected);
+      },
+    );
+  });
+  describe('findHowManyNiceStringsNext', () => {
+    const cases = [
+      { level: 'easy', input: ['abacdef', 'abcdefab', 'qjhvhtzxzqqjkmpb'], expected: 1 },
+      {
+        level: 'medium',
+        input: ['pqrstuvv', 'aabgvyzdsv', 'aaacbgvyzdsv', 'xxyxx', 'aaa'],
+        expected: 1,
+      },
+      {
+        level: 'hard',
+        input: [
+          'uurcxstgmygtbstg',
+          'ieodomkazucvgmuy',
+          'jchzalrnumimnmhp',
+          'haegwjzuvuyypxyu',
+          'dvszwmarrgswjxmb',
+        ],
+        expected: 0,
+      },
+    ];
+
+    it.each(cases)(
+      'should return $expected when $level level with $input',
+      ({ input, expected }) => {
+        // GIVEN
+        // WHEN
+        const result = findHowManyNiceStringsNext(input);
 
         // THEN
         expect(result).toBe(expected);
